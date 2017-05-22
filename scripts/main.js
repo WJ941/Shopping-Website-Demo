72112d1381c093233fe36353ab2c97409963b5f2
wj941 <wangshaosen666@gmail.com>
2017-05-22 20:08:51 +0800

    almost Finished,and slider achive by "margin-left"

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,15 @@ $(function(){
 	// nav_anchor.next().mouseover(function(){
 	// 	$(this).show();
 	// })
+	$("#search_goods").focus(function(){
+		if($(this).val()==="商品名称"){
+			$(this).val("");
+		}
+	})
+	.blur(function(){
+		if($(this).val()===""){
+			$(this).val("商品名称");
+		}
+	});
+
 });
--- scripts/slider.js
+++ scripts/slider.js
@@ -5,13 +5,25 @@ $(function(){
 	var next=$("#next");
 	//下方按钮栏的hover功能
 	slide_btns.hover(function(){
-		slide_btns.removeClass("on");
-		$(this).addClass("on");
-		var marginDistance=$(this).attr("index")*-550;
-		slide_list.css("margin-left",function(){
-			return marginDistance;
-		});
+		showImg($(this).attr("index"));
 	});
+	function showImg(index){
+		var cur=slide_btns.eq(index);
+		cur.addClass("on")
+			.siblings().removeClass("on");
+		slide_list.css("margin-left",function(){
+			return index*-550;
+		});
+	}
+	//自动轮播
+	var carousel=setInterval(function(){
+		var index=1;
+		return function(){
+			if(index===5){index=0;}
+			showImg(index);
+			index++;
+		};
+	}(),2000);
 	//左右箭头的前后功能
 	prev.click(function(){
 		var curSpan=slide_btns.filter(".on");
